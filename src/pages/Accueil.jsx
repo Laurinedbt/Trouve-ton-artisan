e6365fdf384f94d1def0873180dadb41f0449211
Laurine <deberdt.laurine@gmail.com>
2025-10-29 11:56:39 +0100

    Début structure page accueil

--- src/pages/Accueil.jsx
+++ src/pages/Accueil.jsx
@@ -1,6 +1,26 @@
 const Accueil = () => {
     return (
-        <div></div>
+        <div>
+            <header className="home-header">
+                <div>
+                    <h1>Comment trouver mon artisan ? La Région peut vous aider</h1>
+                </div>
+            </header>
+            <main>
+                <section>
+                    <ol>
+                        <li>Choisir la catégorie d'artisanat dans le menu</li>
+                        <li>Choisir un artisan</li>
+                        <li>Le contacter via le formulaire de contact</li>
+                        <li>Une réponse sera apportée sous 48h</li>
+                    </ol>
+                </section>
+
+                <section>
+                    <h1>Les artisans du mois</h1>
+                </section>
+            </main>
+        </div>
     )
 }
 

83cd9a6d3be8d7f4059a6d599061aa1b5fed2861
Laurine <deberdt.laurine@gmail.com>
2025-10-31 15:01:59 +0100

    Finalisation list section

--- src/pages/Accueil.jsx
+++ src/pages/Accueil.jsx
@@ -1,7 +1,7 @@
 const Accueil = () => {
     return (
         <div>
-            <header className="home-header my-4 px-4">
+            <header className="home-header px-4">
                 <div>
                     <hr className="header-line m-0" />
                     <h1>Comment trouver mon artisan ? La Région peut vous aider</h1>
@@ -11,33 +11,49 @@ const Accueil = () => {
             <main>
                 <section className="list-section">
                     <ol>
-                    <div className="info-section">
-                        <hr className="line redline"/>
-                       <li>Choisir la catégorie d'artisanat dans le menu</li> 
-                       <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>
-                       <img src="/img/img-placeholder.svg" alt="Placeholder img" />
+                    <div className="info-section row">
+                        <div className="col-md-6">
+                            <hr className="line redline"/>
+                            <li className="text-section">Choisir la catégorie d'artisanat dans le menu</li> 
+                            <p className="text">Lorem ipsum dolor sit amet, 
+                            consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>
+                        </div>
+                        <div className="img-placeholder col-md-6">
+                            <img src="/img/img-placeholder.svg" alt="Placeholder img" />
+                        </div>
                     </div>
                     
-                    <div className="info-section">
-                        <hr className="line greenline"/>
-                        <li>Choisir un artisan</li>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+                    <div className="info-section row">
+                        <div className="img-placeholder col-md-6">
+                            <img src="/img/img-placeholder.svg" alt="Placeholder img"/>
+                        </div>
+
+                        <div className="col-md-6">
+                            <hr className="line greenline"/>
+                            <li className="text-section">Choisir un artisan</li>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
                             Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
-                       <img src="/img/img-placeholder.svg" alt="Placeholder img" />
+                        </div>
                     </div>
 
-                    <div className="info-section">
-                        <hr className="line blueline"/>
-                        <li>Le contacter via le formulaire de contact</li>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-                       <img src="/img/img-placeholder.svg" alt="Placeholder img" />
+                    <div className="info-section row">
+                        <div className="col-md-6">
+                            <hr className="line blueline"/>
+                            <li className="text-section">Le contacter via le formulaire de contact</li>
+                            <p className="text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                        </div>
+                        
+                        <div className="img-placeholder col-md-6">
+                            <img src="/img/img-placeholder.svg" alt="Placeholder img" />
+                        </div>
                     </div>
                        
-                    <div className="info-section">
-                        <hr className="line greenline"/>
-                        <li>Une réponse sera apportée sous 48h</li>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-                       <img src="/img/img-placeholder.svg" alt="Placeholder img" />
+                    <div className="info-section row">
+                        <div>
+                            <hr className="line greenline"/>
+                            <li className="last-text">Une réponse sera apportée sous 48h</li>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt <br /> ut labore et dolore magna aliqua.</p>
+                        </div>
                     </div>
 
                     </ol>

12327e062726a820b7750991f01f77b6e1270551
Laurine <deberdt.laurine@gmail.com>
2025-11-03 15:52:01 +0100

    Début page liste des artisans - batiment

--- src/pages/Batiment.jsx
+++ src/pages/Batiment.jsx
@@ -1,6 +1,19 @@
 const Batiment = () => {
     return (
-        <div></div>
+        <div>
+            <header>
+                <h1>Liste des artisans</h1>
+            </header>
+
+            <main>
+                <hr className="line green-line"/>
+                <h2>Bâtiment</h2>
+
+                <section>
+
+                </section>
+            </main>
+        </div>
     )
 }
 
